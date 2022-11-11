c701c76a29aeb0a277564ac09119848cf261b24d
Dimitri Merejkowsky <dimitri@dmerej.info>
2022-11-11 16:25:25 +0100

    cs: better comments

--- cs/Ohce.Test/UITest.cs
+++ cs/Ohce.Test/UITest.cs
@@ -9,12 +9,12 @@
             TODO: check that given the following inputs:
              - hello
              - oto
-            - quit
+             - quit
 
-           The following messages are shown to the user:
-            - olleh
-            - oto
-            - That was a palindrome!
+            the following messages are shown to the user:
+             - olleh
+             - oto
+             - That was a palindrome!
 
            */
             Assert.Fail("TODO");
